9cbc1a947c6b6aa6cf72f913daaf76786fd240f5
ndzywork998 <zhang.yi@pitayagames.com>
2024-07-29 16:34:49 +0800

    prettier 测试

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * as UIAccordion from "@/components/ui/accordion"
+export * as UIButton from "@/components/ui/button"
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+
+// import { peerDependencies } from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      name: "ndzy-design", // Sets the name of the generated library.
+      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    rollupOptions: {
+      // external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
+  },
+})
